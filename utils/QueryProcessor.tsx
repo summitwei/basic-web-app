03be25d72e3a21c980be5d38eee224ce813896bd
summitwei <summitwei@gmail.com>
2024-03-14 15:51:50 -0400

    bruh

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   } else if (query.toLowerCase().includes("andrew")) {
     return "Your (my) Andrew ID is elwei.";
   } else if (query.toLowerCase().includes("name")) {
-    return "youre summit";
+    return "summit";
   } 
 
   return "";
